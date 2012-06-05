ddd6960a7b62c7877c86116b1f7c926fcba42532
calmopyrin <Attila@Attila-PC.(none)>
2012-06-05 23:47:04 +0200

    update release version to 1.0.1

--- win/Aboutdlg.h
+++ win/Aboutdlg.h
@@ -19,7 +19,7 @@ public:
 	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
 	{
 		CenterWindow(GetParent());
-		SetWindowText(_T("About WinTedplay 1.0"));
+		SetWindowText(_T("About WinTedplay 1.0.1"));
 		return TRUE;
 	}
 
